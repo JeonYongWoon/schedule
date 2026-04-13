119ee6320d3489d942b4aeafc64d593313af0a67
JeonYongWoon <oioipo22@gmail.com>
2026-04-14 02:38:05 +0900

    ..#

--- src/main/java/com/example/schedule2/entity/schedule.java
+++ src/main/java/com/example/schedule2/entity/schedule.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
 @Table(name = "schedules")
@@ -14,4 +16,18 @@ public class schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String title;      // 일정 제목
+    private String content;    // 일정 내용
+    private String author;     // 작성자명
+    private String password;   // 비밀번호
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public schedule(String title, String content, String author, String password) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.password = password;
+    }
 }
